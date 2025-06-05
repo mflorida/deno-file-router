9a9c17a4cef80caee7fa7b0cc1309da9d15ad2b9
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2025-06-05 10:36:49 -0500

    Added more example routes. Added 'start' task command.

--- routes/+not_a_route.ts
+++ routes/+not_a_route.ts
@@ -1,3 +1,9 @@
+/**
+ * This is not a route since the `route` suffix is not prepended with dot `.`
+ * (it's `not_a_route` insteade of `not_a.route`). If the file was named
+ * `+not_a.route.ts` the url path would be `/not_a/`. The difference is
+ * subtle, but significant.
+ */
 export function GET() {
   return (
     'How did you get here???'
