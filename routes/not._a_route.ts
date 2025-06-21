14f6a06ee99fff0ab2c856c93fc94363e5b9dbb8
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2025-06-21 10:12:14 -0500

    Adding and moving files for route examples; add .editorconfig;

--- routes/not._a_route.ts
+++ routes/not._a_route.ts
@@ -1,7 +1,7 @@
 /**
  * This is not a route since the `route` suffix is not prepended with dot `.`
- * (it's `not_a_route` insteade of `not_a.route`). If the file was named
- * `+not_a.route.ts` the url path would be `/not_a/`. The difference is
+ * (it's `not._a_route` instead of `not._a.route`). If the file was named
+ * `+not._a.route.ts` the url path would be `/not_a/`. The difference is
  * subtle, but significant.
  */
 export function GET() {
